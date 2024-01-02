ed28d124b315279913c28f63a1dcf72ccbc160a5
wostrau <a.ostrovtsov@gmail.com>
2024-01-02 02:44:19 +0100

    adjusted: function for input validation, code style

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 const firstTaskButton = document.getElementById('btn-task-1');
 const secondTaskButton = document.getElementById('btn-task-2');
 
-const numberValidation = number => !isNaN(number) && number !== null && number !== '';
+const isValidInput = (input) => input.trim().length && !isNaN(input);
 const convertNumbers = (number1, number2) => number1.toString(number2);
 const sumNumbers = (number1, number2) => number1 + number2;
 const divisionNumbers = (number1, number2) => number1 / number2;
 
 const firstTaskHandler = () => {
-    const input1 = +prompt('Enter first number:');
-    const input2 = +prompt('Enter second number:');
+    const input1 = prompt('Enter first number:');
+    const input2 = prompt('Enter second number:');
 
-    if (!numberValidation(input1) || !numberValidation(input2) || input2 === 0) {
-        console.log('Invalid enter!');
-        return;
+    if (!isValidInput(input1) || !isValidInput(input2) || +input2 === 0) {
+        return console.log('Invalid enter!');
     }
 
-    const conversionResult = convertNumbers(input1, input2);
+    const conversionResult = convertNumbers(+input1, +input2);
+
     console.log(conversionResult);
 };
 
 const secondTaskHandler = () => {
-    const input1 = +prompt('Enter first number:');
-    const input2 = +prompt('Enter second number:');
+    const input1 = prompt('Enter first number:');
+    const input2 = prompt('Enter second number:');
 
-    if (!numberValidation(input1) || !numberValidation(input2)) {
-        console.log('Invalid enter!');
-        return;
+    if (!isValidInput(input1) || !isValidInput(input2)) {
+        return console.log('Invalid enter!');
     }
 
-    const sumResult = sumNumbers(input1, input2);
-    const divResult = divisionNumbers(input1, input2);
+    const sumResult = sumNumbers(+input1, +input2);
+    const divResult = divisionNumbers(+input1, +input2);
+
     console.log(`${sumResult}, ${divResult}`);
 };
 
